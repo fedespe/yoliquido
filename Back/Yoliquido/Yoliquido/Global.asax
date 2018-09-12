8a9f9be41833914ab5d51a30a1e86399355c1226
LAPTOP-G36SIJ74\Usuario <fsperonip@hotmail.com>
2018-09-12 17:35:10 -0300

    Comenzando proyecto. Front de prueba en carpeta front. Comenzando con la base de datos en mySQL en carpeta basededatos. Comenzando el  proyecto en .net en cartpeta back.

---
+++ Back/Yoliquido/Yoliquido/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Yoliquido.MvcApplication" Language="C#" %>
---
+++ Back/Yoliquido/Yoliquido/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Yoliquido.Startup))]
+namespace Yoliquido
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
